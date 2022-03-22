b0bd5f4c3b35d6447c57873d92f95a71fb3b863a
Gabe Maurer <gabebmaurer@gmail.com>
2022-03-22 13:24:45 -0500

    adding test folder and adding NA logic Issue I am still having: some rows are showing as NA when they should be outputting prices

--- app/stocks.py
+++ app/stocks.py
@@ -12,8 +12,9 @@ class HandleData:
         self.df = pd.DataFrame(columns=["timestamp", "message", "order_id", "side", "price", "size"]) 
 
     def parse_object(self):
-        test_data = self.data[:10]
+        test_data = self.data[:50]
         for x in test_data:
+            reduce_logging = False
             if x["message"] == "A":
                 self.df.loc[len(self.df)] = x
                 if x["side"] == "B":
@@ -21,8 +22,17 @@ class HandleData:
                 if x["side"] == "S":   
                     self.sell_count += int(x["size"]) 
 
-            elif x["message"] == "R":  
-                y = self.df.loc[self.df.order_id == x["order_id"]]
+            elif x["message"] == "R":
+                y = self.df.loc[self.df.order_id == x["order_id"]]  
+                if self.buy_count >= int(self.target_size) and y.to_dict('records')[0]["side"] == "B":
+                    reduce_logging = True
+                    buy_timestamp = x["timestamp"]
+                    reduce_log = f"{buy_timestamp} S NA"
+                elif self.sell_count >= int(self.target_size) and y.to_dict('records')[0]["side"] == "S":
+                    reduce_logging = True
+                    sell_timestamp = x["timestamp"]
+                    reduce_log = f"{sell_timestamp} B NA"
+                
                 size_post_reduction = int(y.to_dict('records')[0]["size"]) - int(x["size"])
                 if y.to_dict('records')[0]["side"] == "B":
                     self.buy_count -= int(x["size"])
@@ -37,42 +47,44 @@ class HandleData:
             timestamp = x["timestamp"]
             index_count = 0
             if self.buy_count >= int(self.target_size) and x["side"] == "B":
-                temp_buy_df = self.df.sort_values("price")
+                reduce_logging = False
+                temp_buy_df = self.df.sort_values("price", ascending=False)
                 money_spent = 0.0
                 target_size_diff = 0
                 increase_b = int(self.target_size)
                 for index, row in temp_buy_df.iterrows():
-                    
-                    increase_b = increase_b- target_size_diff
-                    if increase_b <=0:
-                        break
-                    target_size_diff += int(row["size"])
-
-                    if target_size_diff >= int(self.target_size):
-                        money_spent = money_spent + (increase_b * float(row["price"]))
-                    elif target_size_diff < int(self.target_size):
-                        money_spent = money_spent + (target_size_diff * float(row["price"]))
+                    if row["side"] == "B":
+                        increase_b = increase_b- target_size_diff
+                        if increase_b <=0:
+                            break
+                        target_size_diff += int(row["size"])
+                        
+                        if target_size_diff >= int(self.target_size):
+                            money_spent = money_spent + (increase_b * float(row["price"]))
+                        elif target_size_diff < int(self.target_size):
+                            money_spent = money_spent + (target_size_diff * float(row["price"]))
                 print(f"{timestamp} S {money_spent}")
-            if self.sell_count >= int(self.target_size) and x["side"] == "S":
-                temp_sell_df = self.df.sort_values("price", ascending=False)
+            elif self.sell_count >= int(self.target_size) and x["side"] == "S":
+                reduce_logging = False
+                temp_sell_df = self.df.sort_values("price")
                 money_made = 0.0
                 target_size_diff = 0
                 increase = int(self.target_size)
                 for index, row in temp_sell_df.iterrows():
-                    
-                    increase = increase- target_size_diff
-                    if increase <=0:
-                        break
-                    target_size_diff += int(row["size"])
+                    if row["side"] == "S":
+                        increase = increase- target_size_diff
+                        if increase <=0:
+                            break
+                        target_size_diff += int(row["size"])
 
-                    if target_size_diff >= int(self.target_size):
-                        money_made = money_made + (increase * float(row["price"]))
-                    elif target_size_diff < int(self.target_size):
-                        money_made = money_made + (target_size_diff * float(row["price"]))
-                        
-                    
-
+                        if target_size_diff >= int(self.target_size):
+                            money_made = money_made + (increase * float(row["price"]))
+                        elif target_size_diff < int(self.target_size):
+                            money_made = money_made + (target_size_diff * float(row["price"]))
+                            
                 print(f"{timestamp} B {money_made}")
+            if reduce_logging:
+                print(reduce_log)
 
     def parse_object1(self):
         test_data = self.data[:10]
